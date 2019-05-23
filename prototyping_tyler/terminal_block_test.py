e7a511bbc2c871b87940615059adba597b99aaf0
Tyler Flanagan <flanagantyler42@gmail.com>
2019-05-23 11:15:50 -0400

    changed from dec to binary

--- prototyping_tyler/terminal_block_test.py
+++ prototyping_tyler/terminal_block_test.py
@@ -1,12 +1,17 @@
 import piplates.MOTORplate as MOTOR
 import time
 
-flag = 1  
-while(flag):
+
+MOTOR.intEnable(1) 
+
+flag = 0  
+while(flag == 0):
     time.sleep(0.1)
     stat = MOTOR.getSENSORS(1)
-    if (stat & 0x40):
-        flag = 0
-        print "bump switch activated!"
-    else:
-        print "waiting for activation"
+    "{0:b}".format(stat)
+    #if (stat & 0x40):
+        #flag = 1
+        #print "bump switch activated!"
+    #else:
+        #print "waiting for activation"
+    print(stat)
