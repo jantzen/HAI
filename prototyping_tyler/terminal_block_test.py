bd4c8dae32e54b3396111583baec3fc914a9c54d
Benjamin C. Jantzen <bjantzen@vt.edu>
2019-05-23 13:50:34 -0400

    working bump whiskers

--- prototyping_tyler/terminal_block_test.py
+++ prototyping_tyler/terminal_block_test.py
@@ -1,17 +1,17 @@
 import piplates.MOTORplate as MOTOR
 import time
 
+MOTOR.intEnable(1)
+dummy = MOTOR.getINTflag0(1)
 
-MOTOR.intEnable(1) 
+flag = 0
 
-flag = 0  
 while(flag == 0):
+    
     time.sleep(0.1)
-    stat = MOTOR.getSENSORS(1)
-    "{0:b}".format(stat)
-    #if (stat & 0x40):
-        #flag = 1
-        #print "bump switch activated!"
-    #else:
-        #print "waiting for activation"
-    print(stat)
+    dummy1  = MOTOR.getINTflag0(1)
+    formatdummy = "{0:b}".format(dummy1)
+    if (dummy1 & 0x4):
+        flag = 1
+
+print("bump detected")        
