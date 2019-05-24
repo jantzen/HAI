591a6f70075f941d550ea00d8d9619227e26dcb4
Tyler Flanagan <flanagantyler42@gmail.com>
2019-05-24 12:20:15 -0400

    added whiskers, DACQ whiskers, and updated SML diagram

---
+++ BBR/nodes/sensors/New folder/DACQ_Whisker.py
@@ -0,0 +1,20 @@
+import piplates.DAQCplate as DAQC
+
+class DAQC_Whisker(Whisker):
+    
+    def __init__(self):
+        self._DAQC.intEnable(0)
+        self._DAQC.getDINbit(0,2)
+        
+    def read(self):
+        while(Whisker._isActivated == False):
+            time.sleep(0.1)
+            self._DAQC.getDINbit(0,2)
+            if(self.DAQC.getDINbit(0,2) = 1):
+                Whisker._isActivated = True
+
+        print("bump detected")
+                
+            
+            
+            
---
+++ BBR/nodes/sensors/Whiskers.py
@@ -0,0 +1,42 @@
+#import BumpNode
+import time
+
+
+class Whisker(sensor):
+    """Generic class describing Whisker bump sensors"""
+
+
+    def __init__(self, afferents, isActived = False, signal):
+        self._afferents = [BumpNode,]
+        self._isActivated = False
+        #defult signal strength of board
+        self._voltageSignal = signal
+        
+
+    def read(self):
+        #while True:
+            while(self._isActivated == False):
+                time.sleep(0.1)
+                
+                #if signal strength changes then interrupt current process and move to interrupt handler
+                if(signal > signal):
+                    self._isActivated = True
+            
+            if(self._isActivated = True):
+                #send off info to BumpNode
+                
+    
+
+    def run():
+       self.read()
+       self.emergency_stop()
+          
+
+    def emergency_stop(self):
+        
+        try:
+            self.run()
+
+        except (KeyboardInterrupt, SystemExit):
+            cleanup_stop_thread()
+            sys.exit()
