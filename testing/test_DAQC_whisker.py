bbce4e568cc7cd33c21a6f9d2b58d3f2f7d198b0
Benjamin C. Jantzen <bjantzen@vt.edu>
2019-07-22 11:19:43 -0400

    Modified whisker routines to include clear() method.

--- testing/test_DAQC_whisker.py
+++ testing/test_DAQC_whisker.py
@@ -3,24 +3,29 @@ import multiprocessing as mp
 import time
 import queue
 
-q = mp.Queue(maxsize=1)
-w = DAQC_Whisker([q], 0, 0)
-p = mp.Process(target=w.run)
-try:
-    print("Starting process...")
-    p.start()
-    time.sleep(5.)
-    print("Terminating process...")
-    p.terminate()
-    p.join()
-except:
-    print("  ended in exception")
-
-qsize = q.qsize()
-print("Queue size: {}".format(qsize))
-print("Contents: ")
-for ii in range(qsize):
+def test_bump_sensor():
+    q = mp.Queue(maxsize=1)
+    w = DAQC_Whisker([q], 0, 0)
+    p = mp.Process(target=w.run)
+    qsize = q.qsize()
+    print("Queue size: {}".format(qsize))
+    
     try:
-        print(q.get_nowait())
-    except queue.Empty:
-        print("no items in queue")
+        print("Starting process...")
+        p.start()
+        time.sleep(5.)
+        print("Terminating process...")
+        p.terminate()
+        p.join()
+    except:
+        print("  ended in exception")
+
+    qsize = q.qsize()
+    print("Queue size: {}".format(qsize))
+    print("Contents: ")
+    for ii in range(qsize):
+        try:
+            print(q.get_nowait())
+        except queue.Empty:
+            print("no items in queue")
+    assert qsize == 1
